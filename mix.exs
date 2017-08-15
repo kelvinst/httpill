a48fed9598eb9cce725d2829c61a32aa3ca0caeb
Kelvin Stinghen <kelvin.stinghen@gmail.com>
2017-08-14 21:05:50 -0300

    Configuring package

--- mix.exs
+++ mix.exs
@@ -6,7 +6,19 @@ defmodule HTTPill.Mixfile do
       app: :httpill,
       version: "0.1.0",
       elixir: "~> 1.5",
+      description: "HTTP requests for sick people!",
+      name: "HTTPill",
+      source_url: "https://github.com/kelvinst/httpill",
       start_permanent: Mix.env == :prod,
+
+      package: [
+        name: :httpill,
+        files: ["lib", "mix.exs", "README*", "LICENSE*"],
+        maintainers: ["Kelvin Stinghen"],
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/kelginst/httpill"}
+      ],
+
       deps: deps()
     ]
   end
@@ -21,8 +33,7 @@ defmodule HTTPill.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
     ]
   end
 end
